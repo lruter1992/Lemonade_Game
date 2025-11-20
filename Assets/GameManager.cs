364f3eaf3954345e96ea7d3936e4ae5aacec77f1
LEEDS-ART\thomas.collins <collins.1992@googlemail.com>
2025-11-20 15:04:41 +0000

    Recomitt

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,19 +4,25 @@ using TMPro; //Load in all the libraries
 
 public class GameManager : MonoBehaviour
 {
-    public int lemons;
-    public int Lemonade;
+    [SerializeField]
+	public int lemons;
+    public int lemonade;
+	public int lemonadeCost = 10; //Lemon Cost
+	public int lemonMulti = 2; //Multiplier for the lemons
+	public float speed = 0.1f;
+	public TMP_Text lemonCounterText;
+	public TMP_Text lemonadeCounterText;
     // Void Start is Event Begin Play
     void Start()
     {
-        
+        InvokeRepeating(nameof(AutoLemon), 0.1f, speed);
     }
 
     // Update is EventTick
     void Update()
     {
-        // Debug.Log("Hello World"); //Print String
-        //BuyLemons();
+		lemonCounterText.text = "lemons:" + lemons; //Update LemonText
+		lemonadeCounterText.text = "lemonade:" + lemonade; //Update LemonText
     }
 
     public void BuyLemons()
@@ -24,5 +30,19 @@ public class GameManager : MonoBehaviour
         lemons++;
         Debug.Log(lemons.ToString());
     }
+	public void MakeLemonade()
+	{
+		if(lemons > lemonadeCost)
+		{
+			lemonade++; //Add 1 Lemonade
+			lemons = lemons - lemonadeCost; //Subtract Lemons
+		}
+	}
+	
+	void AutoLemon()
+	{
+		lemons = lemons + lemonMulti;
+	}
+	
 
 }
