2839ed981cbacb41955d78fb7e9a7cf705f375f0
LEEDS-ART\thomas.collins <collins.1992@googlemail.com>
2025-11-20 14:25:49 +0000

    Update_1

---
+++ Assets/ItemData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+[System.Serializable]
+public class ItemData
+{
+    public string itemName;
+    public int price;
+    public string description;
+    public Sprite itemIcon;
+}
