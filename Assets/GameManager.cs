67b92239c0c4f38ab74f6fffccaabd40df204cad
ALIENWARE\Alienware <collins.1992@gmail.com>
2025-11-23 12:48:22 +0000

    Added variables to the GameManager

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using TMPro; //Load in all the libraries 
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
+	public static GameManager I;
+	
+    [Header("Inventory")]
 	public int lemons;
     public int lemonade;
 	public int lemonadeCost = 10; //Lemon Cost
@@ -12,6 +15,36 @@ public class GameManager : MonoBehaviour
 	public float speed = 0.1f;
 	public TMP_Text lemonCounterText;
 	public TMP_Text lemonadeCounterText;
+
+	[Header("Pricing")] [SerializeField] private float pricePerCup = 1.00f;
+	public float PricePerCup => pricePerCup;
+
+	[Header("Sales Tracking")] 
+	public List<int> salesHistory = new List<int>(); //Cups sold per day
+	public List<float> revenueHistory = new List<float>(); // Money Per Day
+
+	public int currentDay = 0;
+	
+	private void Awake()
+	{
+		I = this;
+	}
+
+	public void SetPrice(float newPrice)
+	{
+		pricePerCup = Mathf.Max(0.01f, newPrice); //Avoid Free or Negative
+	}
+
+	public void EndDay(int cupsSold)
+	{
+		cupsSold = Mathf.Clamp(cupsSold, 0, lemonade);
+		lemonade -= cupsSold;
+		float revenue = cupsSold * pricePerCup;
+		salesHistory.Add(cupsSold);
+		revenueHistory.Add(revenue);
+		currentDay++;
+	}
+	
     // Void Start is Event Begin Play
     void Start()
     {
