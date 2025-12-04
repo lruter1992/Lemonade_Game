1b530193ad1794f7d8a46b08fb7dcde0f533bfe9
LEEDS-ART\thomas.collins <collins.1992@googlemail.com>
2025-12-04 11:43:42 +0000

    Added Customer Script, and text input

---
+++ Assets/CustomerScript.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class CustomerScript : MonoBehaviour
+{
+    public GameManager gameManager; //Link to the gamemanager
+    private int LemonadeCost; //Int of Lemonade Cost
+    private int customers; //Int of Customer Numbers
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameManager = GetComponent<GameManager>(); //Automatically get the Component of GameManager
+        LemonadeCost = gameManager.cost; //Get Int from Game Manager
+        customers = gameManager.customers;
+        Debug.Log(CalculateSales());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    int CalculateSales()
+    {
+        int sales = Mathf.Max(customers - LemonadeCost,0);
+        return sales;
+      
+    }
+}
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using TMPro; //Load in all the libraries 
+using UnityEngine.UI; //Make sure to add
 
 public class GameManager : MonoBehaviour
 {
@@ -9,13 +10,22 @@ public class GameManager : MonoBehaviour
     public int lemonade;
 	public int lemonadeCost = 10; //Lemon Cost
 	public int lemonMulti = 2; //Multiplier for the lemons
+	public int cost; // Add me 
+	public int customers; // Add Me
+	public TMP_InputField priceInputField;
 	public float speed = 0.1f;
 	public TMP_Text lemonCounterText;
 	public TMP_Text lemonadeCounterText;
     // Void Start is Event Begin Play
+
+    void Awake()
+    {
+	    customers = Random.Range(0, 100);
+    }
     void Start()
     {
         InvokeRepeating(nameof(AutoLemon), 0.1f, speed);
+       
     }
 
     // Update is EventTick
@@ -42,6 +52,19 @@ public class GameManager : MonoBehaviour
 	void AutoLemon()
 	{
 		lemons = lemons + lemonMulti;
+	}
+
+	public void UpdatePriceFromInput()
+	{
+		if (int.TryParse(priceInputField.text, out int result))
+		{
+			cost = result;
+			Debug.Log("Price updated: " + cost);
+		}
+		else
+		{
+			Debug.LogWarning("Invalid price input!");
+		}
 	}
 	
 
